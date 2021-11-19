a58758730027e0031369f7c66559074202fc1192
1Guardian <xeroxmc6@gmail.com>
2021-11-19 14:55:22 -0600

    Update main.js
    Logging update

--- CS4500 Project/main.js
+++ CS4500 Project/main.js
@@ -1,6 +1,8 @@
 const {app, BrowserWindow} = require('electron')
 const electron = require('electron')
 const path = require('path')
+const log = require('electron-log');
+log.transports.file.resolvePath = () => path.join('./', 'main.log');
 const remote = require('electron').remote;
 
 const MAIN_HTML = path.join('file://', __dirname, '/Sections/TitlePage/index.html');
@@ -15,18 +17,23 @@ const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
       webviewTag: true
     }
   })
+  //handle errors that arise for later logging
+  parent.webContents.on('console-message', (e, level, message, line) => {
+    console.log('Main page logged a message:', message)
+    log.info('Main page logged a message:', message);
+  })
   parent.once('close', () => {
     parent = null;
   });
 
   parent.loadURL(MAIN_HTML);
-  //parent.toggleDevTools();
+  parent.toggleDevTools();
 };
 
 //~ app.on('ready', onAppReady);
 app.on('ready', () => setTimeout(onAppReady, 500));
 
-
+//handle the closing of the app
 app.on('window-all-closed', () => {
   app.quit()
 })
